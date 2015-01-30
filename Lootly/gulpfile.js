05a5e5b982a35be8698a75b92e901adfeff342ed
Bill Rawlinson <bill.rawlinson@sbcs.com>
2015-01-30 09:48:58 -0500

    introduce gulp into the projects workflow

---
+++ Lootly/gulpfile.js
@@ -0,0 +1,33 @@
+﻿// include plug-ins
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+
+var config = {
+	 //Include all js files but exclude any min.js files
+	 src: [
+		  'Scripts/modernizr*.js'
+		  , 'Scripts/jquery-2.1.3.js'
+		  , 'Scripts/jquery.validate.js'
+		  , 'Scripts/jqeruy.validate.unobtrusive.js'
+		  , '!Scripts/*.min.js'
+		  , '!Scripts/bootstrap.js'],
+}
+
+// Synchronously delete the output file(s)
+gulp.task('clean', function () {
+	 del.sync(['Scripts/all.min.js'])
+});
+
+// Combine and minify all files from the app folder
+gulp.task('scripts', ['clean'], function () {
+
+	 gulp.src(config.src)
+	  .pipe(uglify())
+	  .pipe(concat('all.min.js'))
+	  .pipe(gulp.dest('Scripts/'));
+});
+
+//Set a default tasks
+gulp.task('default', ['scripts'], function () { });
